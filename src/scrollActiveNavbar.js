b834c549a007f77ff7575e2ecdef2f47a0b7f194
DumontGontran <dumont.gontran@orange.fr>
2022-10-17 19:35:15 +0200

    active detection on scroll for navbar

---
+++ src/scrollActiveNavbar.js
@@ -0,0 +1,29 @@
+let sections = window.document.querySelectorAll('section')
+let navlinks = window.document.querySelectorAll('nav .products .navlink')
+
+window.onscroll = () => {
+    let current = ''
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop
+
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute('id')
+        }
+    })
+
+    navlinks.forEach((a) => {
+        a.classList.remove('active')
+
+        if (a.classList.contains(current)) {
+            a.classList.add('active')
+        }
+
+        if (a.classList.contains('active')) {
+            a.classList.remove('navlink_hover')
+        }
+        else {
+            a.classList.add('navlink_hover')
+        }
+    })
+}
