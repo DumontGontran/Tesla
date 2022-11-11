53d3df45ddddcd6db4e7879c3d5f9e5369ad0a53
DumontGontran <dumont.gontran@orange.fr>
2022-11-11 18:32:05 +0100

    adding input[range] quantity CSS style + addInInventory customOrder LS

--- src/scripts/order/customOrder.js
+++ src/scripts/order/customOrder.js
@@ -10,8 +10,8 @@ async function getArticleData() {
 
 async function displayArticle() {
   let article = await getArticleData();
-  for (i = 0; i < article[0].colors.length; i++) {
-    document.querySelector('.article').innerHTML = `<form>
+
+  document.querySelector('.article').innerHTML = `<form>
     <h1 class="font-semibold text-4xl m-3 text-white">${article[0].model}</h1>
     <span class="text-2xl m-3 text-white">${article[0].description}</span>
     <div class="flex-row m-2">
@@ -21,19 +21,46 @@ async function displayArticle() {
     </div>
     <div class="flex-row m-2">
       <label for="quantity" class="text-white">Choose the quantity:*</label>
-      <input type="number" name="quantity" id="quantity" value="1" min="1" max="100" class="form_infos w-[8.3rem]" required>
+      <input type="range" name="quantity" id="quantity" value ="1" min="1" max="100" step="1" pattern="[0-9]{3}" class="w-[8.3rem]" required 
+        oninput="this.nextElementSibling.value = this.value" list="tickmarks">
+      <output id="quantity_value">1</output>
+      <datalist id="tickmarks">
+        <option value="1"></option>
+        <option value="25"></option>
+        <option value="50"></option>
+        <option value="75"></option>
+        <option value="100"></option>
+      </datalist>
     </div>
     <span class="text-2xl text-white font-semibold absolute top-2 right-2">${article[0].price}</span>
-    <input type="submit" value="Buy it" class="buttons order cursor-pointer mx-auto w-[18rem]">
+    <input type="submit" value="Buy it" id="submit" class="buttons order cursor-pointer mx-auto w-[18rem]">
+    <p class="error"></p>
   </form>`
 
-    article[0].colors.forEach((color) => {
-      const colors = document.querySelector('#colors');
-      let colorOption = document.createElement('option');
-      colorOption.textContent = `${color}`;
-      colorOption.value = `${color}`;
-      colors.appendChild(colorOption);
-    });
-  }
+  const colors = document.querySelector('#colors');
+
+  article[0].colors.forEach((color) => {
+    let colorOption = document.createElement('option');
+    colorOption.textContent = `${color}`;
+    colorOption.value = `${color}`;
+    colors.appendChild(colorOption);
+  });
+  addInInventory(article, colors, quantity);
 }
 displayArticle();
+
+const addInInventory = (article, colorOption, quantity) => {
+  document.querySelector('#submit').addEventListener('click', (event) => {
+    event.preventDefault();
+
+    customOrder = {
+      id: article[0].id,
+      name: article[0].model,
+      color: colors.value,
+      quantity: quantity.value
+    }
+    console.log(customOrder);
+    localStorage.setItem('customOrder', JSON.stringify([customOrder]));
+    window.location.href = 'http://localhost:5500/src/pages/main_pages/inventory.html';
+  })
+}
